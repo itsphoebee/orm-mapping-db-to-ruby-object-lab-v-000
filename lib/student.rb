7d2c260001b28e49fe2242485db1e023de2bb070
Phoebe Lau <phoebelau7@gmail.com>
2017-12-28 20:44:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,15 +79,6 @@ class Student
     end.first
   end
 
-   +    DB[:conn].execute(sql).map do |row|
-   +      self.new_from_db(row)
-   +    end.first
-   +  end
-
-
-
-
-
   def self.all_students_in_grade_X(n)
     sql = <<-SQL
       SELECT *
